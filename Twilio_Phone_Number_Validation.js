0ade8586fb137955e7418c714105af1172307882
JZinger18 <jzinger18@gmail.com>
2016-10-24 00:35:19 -0400

    Remove API Tokens. Fix array/loop logic

--- Twilio_Phone_Number_Validation.js
+++ Twilio_Phone_Number_Validation.js
@@ -1,38 +1,37 @@
 function lookup(phoneNumber) {
-    var lookupUrl = "https://lookups.twilio.com/v1/PhoneNumbers/" + phoneNumber + "?Type=carrier"; 
-    
+    var lookupUrl = "https://lookups.twilio.com/v1/PhoneNumbers/" + phoneNumber + "?Type=carrier";
     var options = {
-        "method" : "get"
+        "method": "get"
     };
-
-    options.headers = {    
-        "Authorization" : "Basic " + Utilities.base64Encode("ACcf8380b71eef3cc29ac6b53a52bae9ef:86c05faff856dc3512341f386cbe4e6f")
+    options.headers = {
+        "Authorization": "Basic " + Utilities.base64Encode("ACcf8380b71eef3cc29ac6b53a52bae9ef:86170fe2521fe0231e4285abacb8968a")
     };
-  
+    tw
     var response = UrlFetchApp.fetch(lookupUrl, options);
-    var data = JSON.parse(response); 
-    Logger.log(data); 
-    return data; 
+    var data = JSON.parse(response);
+    Logger.log(data);
+    return data;
 }
-
 function lookupAll() {
     var sheet = SpreadsheetApp.getActiveSheet();
-    var startRow = 2; 
-    var numRows = sheet.getLastRow() - 1; 
-    var dataRange = sheet.getRange(startRow, 1, numRows); 
-  
-  // TODO: Exlude Rows if "Twilio Lookup = 'Yes'" from array
+    var resumeCell = sheet.getRange(2, 8).getValue()
+    var startRow = 2;
+    var numRows = sheet.getLastRow() - 1;
+    //var dataRange = sheet.getRange(startRow, 1, numRows); 
+    var dataRange = sheet.getRange(resumeCell, 1, numRows);
+    // TODO: Exlude Rows if "Twilio Lookup = 'Yes'" from array
     var phoneNumbers = dataRange.getValues();
-  
     for (var i in phoneNumbers) {
-        var phoneNumber = phoneNumbers[i]; 
-        var spreadsheetRow = startRow + Number(i); 
-        if (phoneNumber != "" && sheet.getRange(spreadsheetRow, 7).getValue() != 'Yes') { 
-            try { 
+        var phoneNumber = phoneNumbers[i];
+        var spreadsheetRow = startRow + Number(i);
+        if (phoneNumber != "" && sheet.getRange(spreadsheetRow, 7).getValue() != 'Yes') {
+            try {
                 sheet.getRange(spreadsheetRow, 2, spreadsheetRow, 6).setValue(""); // Clears before writing
                 data = lookup(phoneNumber);
-                if (data['status'] == 404) { 
-                    sheet.getRange(spreadsheetRow, 2).setValue("not found");
+                if (data['status'] == 404) {
+                    sheet.getRange(spreadsheetRow, 2).setValue("not found"); // Number not found
+                    resumeCell++;
+                    sheet.getRange(2, 8).setValue(resumeCell + 1)
                 } else {
                     sheet.getRange(spreadsheetRow, 2).setValue("found");
                     sheet.getRange(spreadsheetRow, 3).setValue(data['carrier']['type']);
@@ -40,18 +39,21 @@ function lookupAll() {
                     sheet.getRange(spreadsheetRow, 5).setValue(data['country_code']);
                     sheet.getRange(spreadsheetRow, 6).setValue(data['national_format']);
                     sheet.getRange(spreadsheetRow, 7).setValue('Yes');
-                }  
-            } catch(err) {
+                    resumeCell++;
+                    sheet.getRange(2, 8).setValue(resumeCell + 1)
+                }
+            } catch (err) {
                 Logger.log(err);
-                sheet.getRange(spreadsheetRow, 2).setValue('lookup error');
+                sheet.getRange(spreadsheetRow, 2).setValue('lookup error'); // API callback error
                 sheet.getRange(spreadsheetRow, 7).setValue('No');
             }
         } else {
-          sheet.getRange(spreadsheetRow, 7).setValue('Yes');
+            sheet.getRange(spreadsheetRow, 7).setValue('Yes');
+            resumeCell++;
+            sheet.getRange(2, 8).setValue(resumeCell);
         }
     }
 }
-
 function main() {
-  lookupAll();
+    lookupAll();
 }
